bdc7bc0f54ffdbe9fbf0b53f6c42ee6f22b7f6e1
Sushant Gaikwad <sushantgaikwad06@gmail.com>
2025-04-27 11:03:29 +0530

    watchlater route

--- server.ts
+++ server.ts
@@ -4,24 +4,22 @@ import cors from "cors";
 import authRoutes from "./routes/auth";
 import videoRoutes from "./routes/videos";
 import commentRoutes from "./routes/comments";
-import watchLaterRoutes from './routes/watchLater'; 
+import watchLaterRoutes from "./routes/watchLater";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 
-
-
 // CORS configuration
 app.use(
-    cors({
-      origin: process.env.FRONTEND_URL, // Allow frontend origin
-      methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], // Allowed methods
-      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    })
-  );
-  
+  cors({
+    origin: process.env.FRONTEND_URL, // Allow frontend origin
+    methods: ["GET", "POST", "DELETE", "OPTIONS"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
+
 app.use(express.json());
 
 mongoose
@@ -32,7 +30,7 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
-app.use('/api/watchater', watchLaterRoutes); 
+app.use("/api/watchlater", watchLaterRoutes);
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
